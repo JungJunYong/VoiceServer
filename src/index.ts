ee162c9e17cf10100c884939cb8c313c79e9d105
정준용 <jkouese@gmail.com>
2022-01-17 23:47:41 +0900

    db 연결 예제 작성 완료

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import {$log} from "@tsed/common";
 import { PlatformExpress } from "@tsed/platform-express";
 import {Server} from "./Server";
-import {mariadbConnect} from "./database/connect/mariadb"
 import mariadb from "mariadb";
 
 type database = {
@@ -16,21 +15,6 @@ async function bootstrap() {
   try {
     $log.debug("Start server...");
     const platform = await PlatformExpress.bootstrap(Server);
-    let mariaDB: mariadb.PoolConnection;
-    try {
-      mariaDB = await mariadbConnect();
-    }catch (e) {
-      console.log('mariaDB 연결 실패');
-    }finally {
-      console.log('mariaDB 연결 성공');
-      globalThis.db = {
-        mariadb: mariaDB!
-      }
-    }
-
-
-
-
 
     await platform.listen();
     $log.debug("Server initialized");
