ada2925a85373b346905b14bd495fed7a8ea1bdf
정준용 <jkouese@gmail.com>
2022-03-01 13:03:05 +0900

    디비연결 및 파라미터 가져오기 테스트

--- src/controllers/ChattingControl.ts
+++ src/controllers/ChattingControl.ts
@@ -1,6 +1,7 @@
 import {Controller} from "@tsed/di";
 import {Delete, Get, Post} from "@tsed/schema";
 import {mariadbSql} from "../database/connect/mariadb";
+import {QueryParams} from "@tsed/common";
 
 @Controller("/chat")
 export class ChattingControl {
@@ -13,6 +14,11 @@ export class ChattingControl {
         return await mariadbSql<{ data: string }>('show databases;');
     }
 
+    @Get("/user",["/user/:id"])
+    async getUser(@QueryParams("id") id: string) {
+        return await mariadbSql<USER>(`SELECT * FROM USER WHERE ID='${id}'`);
+    }
+
     @Post("/post")
     post() {
         return {test: '54321'};
@@ -22,6 +28,22 @@ export class ChattingControl {
     delete() {
         return {delete: 'test!!'};
     }
+
+    @Get()
+    findAll() {
+        console.log('findAll')
+        // This endpoint will never get called
+        // because the "/calendars" request is going
+        // to be captured by the "/calendars/:id" route handler
+    }
+}
+
+type USER = {
+    NO: number,
+    ID: string,
+    EMAIL: string,
+    PASSWORD: string,
+    NICNAME: string
 }
 
 
